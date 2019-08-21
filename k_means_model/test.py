68b098beae61d633e2093cbbe080320f60829f12
home-zhaoran2017 <1021300542@qq.com>
2019-08-21 10:25:40 +0800

    添加绘图代码

--- k_means_model/test.py
+++ k_means_model/test.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 from k_means_model_sk import KMeansModel
 
 data = pd.read_csv("data.txt",sep='|',header=None)
@@ -10,3 +12,14 @@ model.fit(data)
 y=model.predict(data)
 score = model.score(data,y)
 print(score)
+
+
+x1 = data[:,0]
+x2 = data[:,1]
+
+plt.figure(dpi=100)
+
+plt.scatter(x1,x2,s=0.5,c=y,cmap="gnuplot")
+plt.colorbar()
+
+plt.savefig("pic.png")
